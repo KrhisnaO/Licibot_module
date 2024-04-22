1eb4ccac6df4f915bd98c74036bb0382b14d14cb
RobertoDuoc98 <129084669+RobertoDuoc98@users.noreply.github.com>
2024-04-22 17:59:30 -0400

    base de datos
    cree la base de datos y puse s.bat y m.bat

--- Licibot/apirest/admin.py
+++ Licibot/apirest/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Licitacion
 
 # Register your models here.
-admin.site.register(Licitacion)
--- Licibot/core/models.py
+++ Licibot/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Licitacion(models.Model):
+    idLicitacion = models.IntegerField(primary_key=True, verbose_name="Id de licitación")
+    nombreLicitacion = models.CharField(max_length=80, blank=False, null=False, verbose_name="Nombre de la licitación")
+
+
+    def __str__(self):
+        return self.nombreLicitacion
